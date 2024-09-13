1fe350c0c0f28626cef200e58923630804363ca9
hondavod70 <hondavod70@gmail.com>
2024-09-13 11:43:01 +0700

    Добавлена обработка TSV-файла

---
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -0,0 +1,84 @@
+import { readFileSync } from 'node:fs';
+
+import { FileReader } from './file-reader.interface.js';
+import { Offer, OfferType } from '../../types/index.js';
+
+export class TSVFileReader implements FileReader {
+  private rawData = '';
+
+  constructor(
+    private readonly filename: string
+  ) { }
+
+  private validateRawData(): void {
+    if (!this.rawData) {
+      throw new Error('File was not read');
+    }
+  }
+
+  private parseRawDataToOffers(): Offer[] {
+    return this.rawData
+      .split('\n')
+      .filter((row) => row.trim().length > 0)
+      .map((line) => this.parseLineToOffer(line));
+  }
+
+  private parseLineToOffer(line: string): Offer {
+    const [
+      name,
+      description,
+      postDate,
+      city,
+      previewImage,
+      images,
+      premium,
+      favorite,
+      rating,
+      type,
+      rooms,
+      guests,
+      price,
+      features,
+      user
+    ] = line.split('\t');
+
+    return {
+      name,
+      description,
+      postDate: new Date(postDate),
+      city,
+      previewImage,
+      images: this.parseImages(images),
+      premium,
+      favorite,
+      rating: this.parseStringToNumber(rating),
+      type: OfferType[type as 'apartment' | 'house' | 'room' | 'hotel'],
+      rooms: this.parseStringToNumber(rooms),
+      guests: this.parseStringToNumber(guests),
+      price: this.parseStringToNumber(price),
+      features: this.parseFeatures(features),
+      user
+    };
+  }
+
+  private parseFeatures(featuresString: string): { name: string }[] {
+    return featuresString.split(';').map((name) => ({ name }));
+  }
+
+  private parseImages(imagesString: string): { name: string }[] {
+    return imagesString.split(';').map((name) => ({ name }));
+  }
+
+  private parseStringToNumber(value: string): number {
+    return Number.parseInt(value, 10);
+  }
+
+  public read(): void {
+    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+  }
+
+  public toArray(): Offer[] {
+    this.validateRawData();
+    return this.parseRawDataToOffers();
+  }
+}
---
+++ src/shared/types/index.ts
@@ -0,0 +1,5 @@
+export { OfferType } from './offer-type.enum.js';
+export { User } from './user.type.js';
+export { Offer } from './offer.type.js';
+export { Image } from './image.type.js';
+
