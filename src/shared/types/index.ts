8d5c70f282a5d6f4638a7bc6a928b1f956aaf635
hondavod70 <hondavod70@gmail.com>
2024-09-23 08:57:27 +0700

    Допилил логгер согласно ДЗ

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,4 +5,6 @@ export { Goods } from './goods.type.js';
 export { Location } from './location.type.js';
 export { City } from './city.type.js';
 export { MockServerData } from './mock-server-data.type.js';
+export { Component } from './component.enum.js';
+
 
