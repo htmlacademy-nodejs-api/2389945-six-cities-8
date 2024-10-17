7c89652fadd81a7dfb374e766589867397c33ff6
hondavod70 <hondavod70@gmail.com>
2024-10-17 21:04:21 +0700

    ++AppExceptionFilter -> RestApplication

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,7 +5,7 @@ import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
-import { Controller } from '../shared/libs/rest/index.js';
+import { Controller, ExceptionFilter } from '../shared/libs/rest/index.js';
 
 @injectable()
 export class RestApplication {
@@ -15,7 +15,9 @@ export class RestApplication {
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient)
-    private readonly databaseClient: DatabaseClient
+    private readonly databaseClient: DatabaseClient,
+    @inject(Component.ExceptionFilter)
+    private readonly appExceptionFilter: ExceptionFilter
   ) {
     this.server = express();
   }
@@ -41,6 +43,12 @@ export class RestApplication {
     this.server.use(express.json());
   }
 
+  private async _initExceptionFilters() {
+    this.server.use(
+      this.appExceptionFilter.catch.bind(this.appExceptionFilter)
+    );
+  }
+
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info('Init database…');
@@ -50,6 +58,10 @@ export class RestApplication {
     this.logger.info('Init app-level middleware');
     await this._initMiddleware();
     this.logger.info('App-level middleware initialization completed');
+
+    this.logger.info('Init exception filters');
+    await this._initExceptionFilters();
+    this.logger.info('Exception filters initialization compleated');
 
     this.logger.info('Try to init server…');
     await this._initServer();
