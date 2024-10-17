89d68311272ce5d6275a2816e78b248bc0b9a27b
hondavod70 <hondavod70@gmail.com>
2024-10-17 20:40:57 +0700

    ++AppExceptionFilter

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,6 +5,7 @@ import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import { Controller } from '../shared/libs/rest/index.js';
 
 @injectable()
 export class RestApplication {
@@ -36,11 +37,19 @@ export class RestApplication {
     this.server.listen(port);
   }
 
+  private async _initMiddleware() {
+    this.server.use(express.json());
+  }
+
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info('Init database…');
     await this.initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init app-level middleware');
+    await this._initMiddleware();
+    this.logger.info('App-level middleware initialization completed');
 
     this.logger.info('Try to init server…');
     await this._initServer();
