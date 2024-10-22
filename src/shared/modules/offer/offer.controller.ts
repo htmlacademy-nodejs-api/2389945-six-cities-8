50ec388e0b26dd1aafaba2b3695654a4fc99a5e8
hondavod70 <hondavod70@gmail.com>
2024-10-22 20:09:55 +0700

    ++index -> OfferController

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -28,6 +28,8 @@ export default class OfferController extends BaseController {
       method: HttpMethod.Get,
       handler: this.show,
     });
+
+    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
   }
 
   public async show(
@@ -46,5 +48,10 @@ export default class OfferController extends BaseController {
     }
 
     this.ok(res, fillDTO(OfferRdo, offer));
+  }
+
+  public async index(_req: Request, res: Response) {
+    const offers = await this.offerService.find();
+    this.ok(res, fillDTO(OfferRdo, offers));
   }
 }
