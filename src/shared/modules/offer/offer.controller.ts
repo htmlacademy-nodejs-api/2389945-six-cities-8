232af2479fafbae057eece18aeeaa785c73b2edd
hondavod70 <hondavod70@gmail.com>
2024-10-25 18:54:14 +0700

    ++middleware -> OfferController

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -14,6 +14,7 @@ import {
   BaseController,
   HttpError,
   HttpMethod,
+  ValidateObjectIdMiddleware,
 } from '../../libs/rest/index.js';
 
 @injectable()
@@ -31,6 +32,7 @@ export default class OfferController extends BaseController {
       path: '/:offerId',
       method: HttpMethod.Get,
       handler: this.show,
+      middlewares: [new ValidateObjectIdMiddleware('offerId')],
     });
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
