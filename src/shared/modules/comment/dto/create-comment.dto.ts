1f70bf4175aaaae86d6aabd8d471bde15dd1e2b1
hondavod70 <hondavod70@gmail.com>
2024-10-26 18:59:56 +0700

    ++CreateCommentDto validation

--- src/shared/modules/comment/dto/create-comment.dto.ts
+++ src/shared/modules/comment/dto/create-comment.dto.ts
@@ -1,7 +1,38 @@
+import {
+  IsMongoId,
+  IsString,
+  Min,
+  Max,
+  MinLength,
+  MaxLength,
+  IsDateString,
+  IsNumber,
+} from 'class-validator';
+import { CreateCommentValidationMessage } from './create-comment.messages.js';
+
 export class CreateCommentDto {
+  @IsMongoId({ message: CreateCommentValidationMessage.offerId.invalidId })
   public offerId: string;
+
+  @MinLength(10, { message: CreateCommentValidationMessage.text.minLength })
+  @MaxLength(100, { message: CreateCommentValidationMessage.text.maxLength })
+  @IsString({ message: CreateCommentValidationMessage.text.invalidFormat })
   public text: string;
+
+  @IsDateString(
+    {},
+    { message: CreateCommentValidationMessage.postDate.invalidFormat }
+  )
   public postDate: Date;
+
+  @IsNumber(
+    { maxDecimalPlaces: 1 },
+    { message: CreateCommentValidationMessage.rating.invalidFormat }
+  )
+  @Min(1, { message: CreateCommentValidationMessage.rating.minValue })
+  @Max(5, { message: CreateCommentValidationMessage.rating.maxValue })
   public rating: number;
-  public email: string;
+
+  @IsMongoId({ message: CreateCommentValidationMessage.userId.invalidId })
+  public userId: string;
 }
