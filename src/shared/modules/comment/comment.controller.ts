e8f1c94565d19673ef368bcb3d6b41dde096fcf4
hondavod70 <hondavod70@gmail.com>
2024-10-27 21:50:50 +0700

    CommentController updated

--- src/shared/modules/comment/comment.controller.ts
+++ src/shared/modules/comment/comment.controller.ts
@@ -36,7 +36,7 @@ export default class CommentController extends BaseController {
   }
 
   public async create(
-    { body }: CreateCommentRequest,
+    { body, tokenPayload }: CreateCommentRequest,
     res: Response
   ): Promise<void> {
     if (!(await this.offerService.exists(body.offerId))) {
@@ -47,7 +47,10 @@ export default class CommentController extends BaseController {
       );
     }
 
-    const comment = await this.commentService.create(body);
+    const comment = await this.commentService.create({
+      ...body,
+      userId: tokenPayload.id,
+    });
     await this.offerService.incCommentCount(body.offerId);
     this.created(res, fillDTO(CommentRdo, comment));
   }
