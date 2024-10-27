f50f41e59221e0f90e355ea4ee6ee48954caafd5
hondavod70 <hondavod70@gmail.com>
2024-10-27 21:08:31 +0700

    ++createAuthContainer

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -23,7 +23,9 @@ export class RestApplication {
     @inject(Component.OfferController)
     private readonly offerController: Controller,
     @inject(Component.CommentController)
-    private readonly commentController: Controller
+    private readonly commentController: Controller,
+    @inject(Component.AuthExceptionFilter)
+    private readonly authExceptionFilter: ExceptionFilter
   ) {
     this.server = express();
   }
@@ -60,6 +62,9 @@ export class RestApplication {
   }
 
   private async _initExceptionFilters() {
+    this.server.use(
+      this.authExceptionFilter.catch.bind(this.authExceptionFilter)
+    );
     this.server.use(
       this.appExceptionFilter.catch.bind(this.appExceptionFilter)
     );
