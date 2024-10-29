97b015635c4c1438fdd81dc3b3976de0f89d9160
hondavod70 <hondavod70@gmail.com>
2024-10-29 21:10:42 +0700

    ++upload directory

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -67,9 +67,15 @@ export class RestApplication {
     );
 
     this.server.use(express.json());
+
     this.server.use(
       '/upload',
       express.static(this.config.get('UPLOAD_FILE_DIRECTORY'))
+    );
+
+    this.server.use(
+      '/static',
+      express.static(this.config.get('STATIC_DIRECTORY_PATH'))
     );
     this.server.use(
       authenticateMiddleware.execute.bind(authenticateMiddleware)
