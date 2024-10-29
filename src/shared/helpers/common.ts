0f3bf02d30f5f94dc4ec95530709b774a15c5ebf
hondavod70 <hondavod70@gmail.com>
2024-10-29 19:43:07 +0700

    ++reduceValidationErrors

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,4 +1,6 @@
 import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { ValidationError } from 'class-validator';
+import { ValidationErrorField } from '../libs/rest/index.js';
 
 export function generateRandomValue(
   min: number,
@@ -41,4 +43,14 @@ export function createErrorObject(message: string) {
   return {
     error: message,
   };
+}
+
+export function reduceValidationErrors(
+  errors: ValidationError[]
+): ValidationErrorField[] {
+  return errors.map(({ property, value, constraints }) => ({
+    property,
+    value,
+    messages: constraints ? Object.values(constraints) : [],
+  }));
 }
