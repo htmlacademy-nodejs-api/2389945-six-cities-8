a3a8fd0f83eb610a8ee069c4c8d853669bebe47d
hondavod70 <hondavod70@gmail.com>
2024-10-30 23:26:59 +0700

    ++CORS -> middleware

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -6,6 +6,7 @@ import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getFullServerPath, getMongoURI } from '../shared/helpers/index.js';
 import { STATIC_FILES_ROUTE, STATIC_UPLOAD_ROUTE } from './rest.constant.js';
+import cors from 'cors';
 import {
   Controller,
   ExceptionFilter,
@@ -78,9 +79,12 @@ export class RestApplication {
       STATIC_FILES_ROUTE,
       express.static(this.config.get('STATIC_DIRECTORY_PATH'))
     );
+
     this.server.use(
       authenticateMiddleware.execute.bind(authenticateMiddleware)
     );
+
+    this.server.use(cors());
   }
 
   private async _initExceptionFilters() {
