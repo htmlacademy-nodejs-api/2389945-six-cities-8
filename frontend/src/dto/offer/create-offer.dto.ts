dcb59743222ce2e3f1e96960f66dc0f33553730b
hondavod70 <hondavod70@gmail.com>
2024-10-31 10:43:23 +0700

    ++frontend

---
+++ frontend/src/dto/offer/create-offer.dto.ts
@@ -0,0 +1,19 @@
+import { City, Location, Type } from '../../types/types';
+
+export default class CreateOfferDto {
+  public title!: string;
+  public description!: string;
+  public postDate!: Date;
+  public city!: City;
+  public previewImage!: string;
+  public images!: string[];
+  public isPremium!: boolean;
+  public rating!: number;
+  public type!: Type;
+  public rooms!: number;
+  public guests!: number;
+  public price!: number;
+  public goods!: string[];
+  public userId!: string;
+  public location!: Location;
+}
